649ff9fe1d3437fc8b6c4ddaf19a943bbcaae91f
clockiz <clockiz@example.com>
2023-07-23 21:25:37 +0200

    added components to App, added Cart and SideBar contexts

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,13 @@ import Footer from './components/Footer';
 const App = () => {
   return <div className='overflow-hidden'>
     <Router>
+      <Header/>
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/product/:id' element = {<ProductDetails/>}/>
       </Routes>
+      <Sidebar/>
+      <Footer/>
     </Router>
   </div>;
 };
