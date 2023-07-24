111b028b0a4bd2b35d76bf15b89f3ee62c15d462
clockiz <isdrozhilkin@gmail.com>
2023-07-24 11:06:31 +0200

    console log to test fetched data

--- src/contexts/ProductContext.jsx
+++ src/contexts/ProductContext.jsx
@@ -12,6 +12,7 @@ const ProductProvider = ({ children }) => {
       const response = await fetch('https://fakestoreapi.com/products');
       const data = await response.json();
       setProdcuts(data);
+      console.log(data);
     };
     fetchProducts();
   }, [])
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
       <div className='container mx-auto'>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0'>
           {filteredProducts.map((product) => {
-            return <div className='w-full h-[300px] bg-pink-200' key = {product.id}>{product.title}</div>;
+            return <div className='w-full h-[300px] bg-pink-200' key={product.id}>{product.title}</div>;
           })}
         </div>
       </div>
