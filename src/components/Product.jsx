f93f16abf6ab4c18616fd638115659e4d7d7e41a
clockiz <102153262+igd93@users.noreply.github.com>
2023-07-24 21:57:17 +0200

    started work on a product

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { BsPlus, BsEyeFill } from 'react-icons/bs';
 
-const Product = () => {
-  return <div>Product</div>;
+const Product = ({ product }) => {
+  //destructure product  
+  const { id, image, category, title, price } = product;
+  return (
+    <div>
+      <div className='border border-[#e4e4e4] h-300px'>1</div>
+      <div>2</div>
+    </div>)
 };
 
 export default Product;
