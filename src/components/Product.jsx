bd2f8f1f6b98d1150cd3980913f051579ef6a285
clockiz <102153262+igd93@users.noreply.github.com>
2023-07-24 22:36:35 +0200

    added bsPlus, bsEyeFill and hovering effects

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -9,14 +9,30 @@ const Product = ({ product }) => {
     <div>
       <div className='border border-[#e4e4e4] h-[300px] mb-4
       relative overflow-hidden group transition'>
-        <div className = 'w-full h-full flex justify-center items-center'>
+        <div className='w-full h-full flex justify-center items-center'>
           {/* images */}
           <div className='w-[200px] mx-auto flex justify-center items-center'>
             <img
-              className='max-h-[160px] group-hover:scale-110'
-              src={image} 
+              className='max-h-[160px] group-hover:scale-110
+              transition duration-300'
+              src={image}
               alt="" />
           </div>
+        </div>
+        {/* buttons */}
+        <div className='absolute top-6 -right-11 group-hover:right-5
+        p-2 flex flex-col items-center justify-center gap-y-2 
+        opacity-0 group-hover:opacity-100 transition-all duration-300'>
+          <button>
+            <div className='flex justify-center items-center text-white 
+            w-12 h-12 bg-red-500'>
+              <BsPlus className='text-3xl' />
+            </div>
+          </button>
+          <Link to={'/'} className='w-12 h-12 bg-white flex justify-center
+          items-center text-primary drop-shadow-x1'>
+            <BsEyeFill/>
+          </Link>
         </div>
       </div>
       <div>2</div>
