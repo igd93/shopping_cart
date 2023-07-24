f5ce480b49ebedebbe9e76ec0c8fa6c8c70d0da5
clockiz <102153262+igd93@users.noreply.github.com>
2023-07-24 22:26:03 +0200

    fixed a semicolon render issue

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -7,7 +7,18 @@ const Product = ({ product }) => {
   const { id, image, category, title, price } = product;
   return (
     <div>
-      <div className='border border-[#e4e4e4] h-300px'>1</div>
+      <div className='border border-[#e4e4e4] h-[300px] mb-4
+      relative overflow-hidden group transition'>
+        <div className = 'w-full h-full flex justify-center items-center'>
+          {/* images */}
+          <div className='w-[200px] mx-auto flex justify-center items-center'>
+            <img
+              className='max-h-[160px] group-hover:scale-110'
+              src={image} 
+              alt="" />
+          </div>
+        </div>
+      </div>
       <div>2</div>
     </div>)
 };
--- src/contexts/ProductContext.jsx
+++ src/contexts/ProductContext.jsx
@@ -5,20 +5,19 @@ export const ProductContext = createContext();
 
 const ProductProvider = ({ children }) => {
   //products state
-  const [products, setProdcuts] = useState([]);
+  const [products, setProducts] = useState([]);
   //fetch products
   useEffect(() => {
     const fetchProducts = async () => {
       const response = await fetch('https://fakestoreapi.com/products');
       const data = await response.json();
-      setProdcuts(data);
+      setProducts(data);
       console.log(data);
-    };
+    }
     fetchProducts();
   }, [])
-  return <ProductContext.Provider value={{ products }}>
-    {children}
-  </ProductContext.Provider>;
+  return <ProductContext.Provider value={ {products} }>
+    {children}</ProductContext.Provider>;
 };
 
 export default ProductProvider;
