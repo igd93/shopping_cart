b937e57ea7af0db7eb5a0fc24a56fb751d01f547
clockiz <102153262+igd93@users.noreply.github.com>
2023-07-24 22:47:04 +0200

    added link to a selected product via eyefill and text added title, category and price styled accordingly

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -29,13 +29,22 @@ const Product = ({ product }) => {
               <BsPlus className='text-3xl' />
             </div>
           </button>
-          <Link to={'/'} className='w-12 h-12 bg-white flex justify-center
+          <Link to={`/product/${id}`} className='w-12 h-12 bg-white flex justify-center
           items-center text-primary drop-shadow-x1'>
-            <BsEyeFill/>
+            <BsEyeFill />
           </Link>
         </div>
       </div>
-      <div>2</div>
+      {/* category & title & price*/}
+      <div>
+        <div className='text-sm capitalize text-gray-500 mb-1'>
+          {category}
+        </div>
+        <Link to = {`/product/${id}`}>
+          <h2 className='font-semibold mb-1'>{title}</h2>
+        </Link>        
+        <div className='font-semibold'>$ {price}</div>
+      </div>
     </div>)
 };
 
