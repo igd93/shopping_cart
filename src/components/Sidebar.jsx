dc76bcc91815edd6343029375b2d42f91b26046e
igd93 <102153262+igd93@users.noreply.github.com>
2023-07-28 17:22:46 +0200

    added sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
+//import Link
+import { Link } from 'react-router-dom';
+//import icons
+import {IoMdArrowForward} from 'react-icons/io';
+import { FiTrash2 } from 'react-icons/fi';
+//import components
+import CartItem from '../components/CartItem';
+//import sidebar context
+import { SidebarContext } from '../contexts/SideBarContext';
+
 
 const Sidebar = () => {
+  const {isOpen, handleClose} = useContext(SidebarContext);
   return <div>Sidebar</div>;
 };
 
