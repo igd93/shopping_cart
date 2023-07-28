baf6a9365ab7a236653178567d8fc6d888053eb2
igd93 <102153262+igd93@users.noreply.github.com>
2023-07-28 17:10:10 +0200

    added sidebar context

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import ProductProvider from './contexts/ProductContext';
+//sidebar provider
+import SidebarProvider from './contexts/SideBarContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <SidebarProvider>
   <ProductProvider>
     <React.StrictMode>
       <App />
     </React.StrictMode>
   </ProductProvider>
+  </SidebarProvider>
 );
