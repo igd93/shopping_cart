6c513a47012225e49c8c6871b187d3a141d3af7a
igd93 <102153262+igd93@users.noreply.github.com>
2023-07-28 19:34:59 +0200

    now when adding items to the cart,  it renders names of the items

--- src/contexts/SideBarContext.jsx
+++ src/contexts/SideBarContext.jsx
@@ -2,14 +2,14 @@ import React, { useState, createContext } from 'react';
 
 export const SidebarContext = createContext()
 
-const SidebarProvider = ({ children }) => {  
+const SidebarProvider = ({ children }) => {
   //sidebar state
   const [isOpen, setIsOpen] = useState(false);
 
   const handleClose = () => {
     setIsOpen(false);
   };
-  return <SidebarContext.Provider value={{isOpen, setIsOpen, handleClose}}>
+  return <SidebarContext.Provider value={{ isOpen, setIsOpen, handleClose }}>
     {children}
   </SidebarContext.Provider>;
 };
