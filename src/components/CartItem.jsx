ee77ec953056a199a5bcdd7a1b13e96b53b13d88
igd93 <102153262+igd93@users.noreply.github.com>
2023-07-29 21:06:09 +0200

    adding an item to cart; shows a picture; shows a description

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const CartItem = () => {
-  return <div>CartItem</div>;
+//import icons
+import {IoMdClose} from 'react-icons/io';
+
+const CartItem = ({ item }) => {
+  // destructure item
+  const { id, title, image, price, anount } = item;
+  return ( <div className='flex'>
+    <div className='w-full min-h-[150px] flex items-center gap-x-4'>
+      {/* image */}
+      <Link to={`/product/${id}`}>
+      <div>
+        <img className='max-w-[80px]' src={image} alt="" />
+      </div>
+      </Link>
+      <div>
+        {/* title & remove icon*/}
+        <div>
+          <Link to = {`product/${id}`} className='text-sm uppercase font-medium max-w-[240px] text-primary hover:underline'>{title}</Link>
+        </div>
+      </div>
+    </div>
+  </div>
+   
+  );
 };
 
 export default CartItem;
