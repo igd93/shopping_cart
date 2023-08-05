f779cdf42a3d80b21420109c78351c66788dd53d
igd93 <102153262+igd93@users.noreply.github.com>
2023-08-05 19:46:57 +0200

    added quantity, price, total amount increase/decrease quanity buttons todo: event listeners to make the buttons interactive

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -2,28 +2,57 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 //import icons
-import {IoMdClose} from 'react-icons/io';
+import { IoMdAdd, IoMdClose, IoMdRemove } from 'react-icons/io';
 
 const CartItem = ({ item }) => {
   // destructure item
-  const { id, title, image, price, anount } = item;
-  return ( <div className='flex'>
+  const { id, title, image, price, amount } = item;
+  return (<div className='flex gap-x-4 py-2 lg:px-6 border-b border-gray-200 w-full font-light text-gray-500'>
     <div className='w-full min-h-[150px] flex items-center gap-x-4'>
       {/* image */}
       <Link to={`/product/${id}`}>
-      <div>
-        <img className='max-w-[80px]' src={image} alt="" />
-      </div>
-      </Link>
-      <div>
-        {/* title & remove icon*/}
         <div>
-          <Link to = {`product/${id}`} className='text-sm uppercase font-medium max-w-[240px] text-primary hover:underline'>{title}</Link>
+          <img className='max-w-[80px]' src={image} alt="" />
+        </div>
+      </Link>
+      <div className='w-full flex flex-col'>
+        {/* title & remove icon*/}
+        <div className='flex justify-between mb-2'>
+          {/* title */}
+          <Link to={`product/${id}`} className='text-sm uppercase 
+          font-medium max-w-[240px] text-primary hover:underline'>
+            {title}
+          </Link>
+          {/* remove icon */}
+          <div className='text-xl cursor-pointer'>
+            <IoMdClose className='text-gray-500 hover:text-red-500 transition' />
+          </div>
+        </div>
+        <div className='flex gap-x-2 h-[36px] text-sm'>
+          {/* quantity */}
+          <div className='flex flex-1 max-w-[100px]
+          items-center h-full border text-primary font-medium'>
+            <div className='flex-1 flex justify-center items-center cursor-pointer'>
+              {/* minus icon */}
+              <IoMdRemove />
+            </div>
+            <div className='h-full flex justify-center items-center px-2'>{amount}</div>
+            <div className='flex-1 flex justify-center items-center cursor-pointer'>
+              {/* plus icon */}
+              <IoMdAdd />
+            </div>
+          </div>
+          {/* item price */}
+          <div className='flex-1 flex items-center justify-around'>$ {price}</div>
+          {/* final price */}
+          <div className='flex-1 flex justify-end items-center text-primary font-medium'>
+            {`$ ${parseFloat(price * amount).toFixed(2)}`}
+          </div>
         </div>
       </div>
     </div>
   </div>
-   
+
   );
 };
 
