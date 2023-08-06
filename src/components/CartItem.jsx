5c394f14b206252c11319832a25a2afe4a066be3
igd93 <102153262+igd93@users.noreply.github.com>
2023-08-06 19:02:40 +0200

    made add/decrease amount button interactive amount and price adjust correspondingly

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -7,7 +7,7 @@ import { IoMdAdd, IoMdClose, IoMdRemove } from 'react-icons/io';
 import { CartContext } from '../contexts/CartContext';
 
 const CartItem = ({ item }) => {
-  const { removeFromCart } = useContext(CartContext);
+  const { removeFromCart, increaseAmount, decreaseAmount } = useContext(CartContext);
   // destructure item
   const { id, title, image, price, amount } = item;
   return (<div className='flex gap-x-4 py-2 lg:px-6 border-b border-gray-200 w-full font-light text-gray-500'>
@@ -36,12 +36,13 @@ const CartItem = ({ item }) => {
           {/* quantity */}
           <div className='flex flex-1 max-w-[100px]
           items-center h-full border text-primary font-medium'>
-            <div className='flex-1 flex justify-center items-center cursor-pointer'>
+            <div onClick = {() => decreaseAmount(id) }className='flex-1 flex justify-center items-center cursor-pointer'>
               {/* minus icon */}
               <IoMdRemove />
             </div>
             <div className='h-full flex justify-center items-center px-2'>{amount}</div>
-            <div className='flex-1 flex justify-center items-center cursor-pointer'>
+            <div onClick = {()=> increaseAmount(id)} 
+              className='flex-1 flex justify-center items-center cursor-pointer'>
               {/* plus icon */}
               <IoMdAdd />
             </div>
