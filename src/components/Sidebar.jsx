9dd77ea9c2853196af2df1a61b8c95e1c5e469ed
igd93 <102153262+igd93@users.noreply.github.com>
2023-08-06 20:13:06 +0200

    total amount shown is now calculated based on the content of the cart

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -14,7 +14,7 @@ import { CartContext } from '../contexts/CartContext';
 
 const Sidebar = () => {
   const { isOpen, handleClose } = useContext(SidebarContext);
-  const { cart, clearCart } = useContext(CartContext);
+  const { cart, clearCart, total } = useContext(CartContext);
   return (
     <div className={`${isOpen ? 'right-0' : '-right-full'} w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] 
   xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}>
@@ -28,7 +28,8 @@ const Sidebar = () => {
           <IoMdArrowForward className='text-2xl' />
         </div>
       </div>
-      <div>
+      <div className='flex flex-col gap-y-2 h-[520px] lg:h-[640px] 
+        overflow-y-auto overflow-x-hidden border-b'>
         {cart.map((item) => {
           return <CartItem item={item} key={item.id} />
         })}
@@ -37,7 +38,7 @@ const Sidebar = () => {
         <div className='flex w-full justify-between items-center'>
           {/* total */}
           <div className='uppercase font-semibold'>
-            <span className='mr-2'>Total: </span> $ 1000
+            <span className='mr-2'>Total: </span> $ {parseFloat(total).toFixed(2)}
           </div>
           {/* clear cart icon*/}
           <div onClick={clearCart} className='cursor-pointer py-4 bg-red-500 
